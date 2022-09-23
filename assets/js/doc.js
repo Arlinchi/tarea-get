42132fd80ba9d0e126f52519142e2c62887630a2
Arlinchi <112575940+Arlinchi@users.noreply.github.com>
2022-09-22 23:44:00 -0300

    Add files via upload

---
+++ assets/js/doc.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+
+    $.get({
+        url: 'https://reqres.in/api/users', //DIRECCIÓN SERVER
+        data: JSON, //Optativo
+        dataType: 'json', //Optativo
+        success: function (respuesta) {
+            console.table(respuesta);
+        },
+        error: function () {
+            console.error("Sin Respuesta");
+        }
+    });
+
+    $('#agregar').click(function () {
+        console.log("agregar");
+        $.get('https://my-json-server.typicode.com/alaravena/ldp3101/usuarios',
+            function (data) {
+              
+                $.each(data, function (i, item) {
+                    $('#tabla').append(
+                        '<tr><td>' + item.id + '</td><td>' +
+                        item.nombre +
+                        '</td><td>' +
+                        item.email + '</td><td>' + '</td><td>' +
+                        item.edad +
+                        '</td><td><img src=' +
+                        item.avatar
+                        + '></td><td><button id="eliminar" class="eliminar" type="button">Eliminar</button></td></tr>');
+                });
+                
+                $('.eliminar').click(function () {
+                    var index = $('.eliminar').index(this) + 1
+                    console.log(index);
+                    var probando = $('tr');
+                    probando[index].remove();
+                });
+
+            });
+    });
+
+
+});
+
+
+
+
+
+
+
+
+
+
