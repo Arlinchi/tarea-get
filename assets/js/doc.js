2e87beee9fb1b24e71c4f34e06b42d8565dbaf72
Arlinchi <ar.guzman@duocuc.cl>
2022-10-03 19:34:10 -0300

    Update doc.js

--- assets/js/doc.js
+++ assets/js/doc.js
@@ -27,28 +27,30 @@ $(document).ready(function () {
                         '</td><td><img src=' +
                         item.avatar
                         + '></td><td><button id="eliminar" class="eliminar" type="button">Eliminar</button></td><td><button type="button" class="btn btn-primary btn-modal" data-bs-toggle="modal" data-bs-target="#exampleModal">Modal</button></td></tr>');
-                        $('.eliminar').click(function () {
-                            var index = $('.eliminar').index(this) + 1
-                            console.log(index);
-                            var probando = $('tr');
-                            probando[index].remove();
-                        });
+                    /*$('.eliminar').click(function () {
+                        var index = $('.eliminar').index(this) + 1
+                        console.log(index);
+                        var probando = $('tr');
+                        probando[index].remove();
+                    });*/
+                    var eliminar = $('.eliminar');
+                    eliminar.click(function(){
+                        $(this).parents('tr').remove();
+                    });
+                    
+                    var id = item.id;
+                    var nombre = item.nombre;
+                    var email = item.email;
+                    var edad = item.edad;
+                    var modalbody = $('.modal-body');
 
-                        var id = item.id;
-                        var nombre = item.nombre;
-                        var email = item.email;
-                        var edad = item.edad;
-                        var modalbody = $('.modal-body');
-
-                $('.btn-modal').click(function(){
-                    $('tr').show(modalbody.text(id + ' ' + nombre + ' ' + email + ' ' + edad));
+                    $('.btn-modal').click(function () {
+                        $('tr').show(modalbody.text(id + ' ' + nombre + ' ' + email + ' ' + edad));
+                    });
                 });
-                });
-
-
-           });
+            });
     });
- 
+
 });
 
 
